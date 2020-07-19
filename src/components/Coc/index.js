e47e8361254a6cd9908747ef60d6548742439835
Ryosuke <ry0suke.uch1tate@gmail.com>
2020-07-19 22:34:00 +0900

    Fix bug

--- src/components/Coc/index.js
+++ src/components/Coc/index.js
@@ -14,29 +14,23 @@ const Coc = () => (
       <br />
       JJUG幹事会は，参加者がハラスメントをしたと判断した場合，当該人物に対して注意，強制退場など，適切な行動をとります。
     </p>
-    <p>
-      以下のような場合，すぐにJJUG幹事会の腕章をつけたスタッフに連絡してください。見つからない場合，イベントTシャツを着たボランティアスタッフに声をかければ取り継ぎます。
-      <ul>
-        <li>ハラスメントにあった</li>
-        <li>他の人がハラスメントにあっている</li>
-        <li>その他なにか気になることがあった</li>
-      </ul>
-    </p>
-    <p>
-      以下の方法でも連絡できます。
-      <ul>
-        <li>＜メールアドレス＞</li>
-        <li>＜Google Form＞</li>
-      </ul>
-    </p>
-    <p>
-      JJUG幹事会は，ハラスメントにあった方が，イベント中に安全かつ安心と感じられるように，以下のような対応を行います。
-      <ul>
-        <li>近隣の警察に連絡する</li>
-        <li>付き添い者の提供</li>
-        <li>その他の必要な支援</li>
-      </ul>
-    </p>
+    <span>以下のような場合，すぐにJJUG幹事会の腕章をつけたスタッフに連絡してください。見つからない場合，イベントTシャツを着たボランティアスタッフに声をかければ取り継ぎます。</span>
+    <ul>
+      <li>ハラスメントにあった</li>
+      <li>他の人がハラスメントにあっている</li>
+      <li>その他なにか気になることがあった</li>
+    </ul>
+    <p>以下の方法でも連絡できます。</p>
+    <ul>
+      <li>＜メールアドレス＞</li>
+      <li>＜Google Form＞</li>
+    </ul>
+    <p>JJUG幹事会は，ハラスメントにあった方が，イベント中に安全かつ安心と感じられるように，以下のような対応を行います。</p>
+    <ul>
+      <li>近隣の警察に連絡する</li>
+      <li>付き添い者の提供</li>
+      <li>その他の必要な支援</li>
+    </ul>
     <p><b>JJUG幹事会は，誰もが安心してJJUG CCCに参加できることを大切にしています。</b></p>
     <hr />
     <p>
@@ -51,16 +45,12 @@ const Coc = () => (
       <br/>
       if a participant engages in harassing behavior, JJUG may take any action they deem appropriate, including warning the offender or expulsion from the conference with no refund.if a participant engages in harassing behavior, JJUG may take any action they deem appropriate, including warning the offender or expulsion from the conference with no refund.
     </p>
-    <p>
-      If you are being harassed, notice somebody else is being harassed, or have any other concerns, please contact a member of JJUG staff immediately. The staff can be identified as they are wearing an armband. If you do not find any of them, ask someone wearing an event T-shirt.
-      <ul>
-        <li>＜メールアドレス＞</li>
-        <li>＜Google Form＞</li>
-      </ul>
-    </p>
-    <p>
-      JJUG will be happy to help participants contact hotel/venue security or local law enforcement, provide escorts, or otherwise assist those experiencing harassment to feel safe for the duration of the event.
-    </p>
+    <p>If you are being harassed, notice somebody else is being harassed, or have any other concerns, please contact a member of JJUG staff immediately. The staff can be identified as they are wearing an armband. If you do not find any of them, ask someone wearing an event T-shirt.</p>
+    <ul>
+      <li>＜メールアドレス＞</li>
+      <li>＜Google Form＞</li>
+    </ul>
+    <p>JJUG will be happy to help participants contact hotel/venue security or local law enforcement, provide escorts, or otherwise assist those experiencing harassment to feel safe for the duration of the event.</p>
     <p><b>We value your attendance.</b></p>
   </div>
 );
