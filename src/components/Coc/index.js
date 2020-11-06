c6744c6936b7c212f46ef590108f2128f7be659d
Ryosuke <hello.ryosuke.u@gmail.com>
2020-11-07 08:48:04 +0900

    Revert "Fix CoC"
    This reverts commit cd9afb7269f373ae031c2352ce26397a77da3daa.

--- src/components/Coc/index.js
+++ src/components/Coc/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Coc = () => (
   <div>
     <p>
-      JJUG CCCオンラインに参加するにあたり，すべての参加者は以下の行動規範に同意していただく必要があります。
+      JJUG CCCに参加するにあたり，すべての参加者は以下の行動規範に同意していただく必要があります。
       <br />
       JJUG幹事会は，JJUG CCCにおいて，この規範を実施します。
     </p>
@@ -14,43 +14,42 @@ const Coc = () => (
       <br />
       JJUG幹事会は，参加者がハラスメントをしたと判断した場合，当該人物に対して注意，強制退場など，適切な行動をとります。
     </p>
-    <span>次のような場合，すぐにJJUG幹事会スタッフに連絡してください。</span>
+    <span>以下のような場合，すぐにJJUG幹事会の腕章をつけたスタッフに連絡してください。見つからない場合，イベントTシャツを着たボランティアスタッフに声をかければ取り継ぎます。</span>
     <ul>
       <li>ハラスメントにあった</li>
       <li>他の人がハラスメントにあっている</li>
-      <li>ハラスメントについて、なにか気になることがあった</li>
+      <li>その他なにか気になることがあった</li>
     </ul>
-    <p>JJUG幹事会スタッフには次の方法で連絡できます。</p>
+    <p>以下の方法でも連絡できます。</p>
     <ul>
-      <li>メール: jjug.ccc@gmail.com</li>
-      <li>公開Webフォーム: <a href="https://forms.gle/XtFQiZbBwmKeS7h39" target="_blank" rel="noopener noreferrer">https://forms.gle/XtFQiZbBwmKeS7h39</a></li>
+      <li>jjug.ccc@gmail.com</li>
     </ul>
-    <p>JJUG幹事会は，ハラスメントにあった方が，イベント中に安全かつ安心と感じられるように，次のような対応を行います。</p>
+    <p>JJUG幹事会は，ハラスメントにあった方が，イベント中に安全かつ安心と感じられるように，以下のような対応を行います。</p>
     <ul>
-      <li>警察に連絡する</li>
+      <li>近隣の警察に連絡する</li>
+      <li>付き添い者の提供</li>
       <li>その他の必要な支援</li>
     </ul>
-    <p>JJUG幹事会は，誰もが安心してJJUG CCCに参加できることを大切にしています。</p>
+    <p><b>JJUG幹事会は，誰もが安心してJJUG CCCに参加できることを大切にしています。</b></p>
     <hr />
     <p>
-      All attendees, speakers, sponsors, and volunteers at JJUG CCC online are required to agree with the following code of conduct.
+      All attendees, speakers, sponsors and volunteers at JJUG CCC are required to agree with the following code of conduct.
       <br />
       Japan Java User Group (JJUG) will enforce this code throughout the event.
     </p>
     <p>
       Participants asked to stop any harassing behavior are expected to comply immediately.
       <br />
-      Harassment includes offensive verbal comments related to gender, race, physical appearance, stalking, or any behaviors of an offensive nature.
+      Harassment includes offensive verbal comments related to gender, race, physical appearance , stalking, or any behaviors of an offensive nature.
       <br/>
-      If a participant engages in harassing behavior, JJUG may take any action they deem appropriate, including warning the offender or expulsion from the conference with no refund.
+      if a participant engages in harassing behavior, JJUG may take any action they deem appropriate, including warning the offender or expulsion from the conference with no refund.if a participant engages in harassing behavior, JJUG may take any action they deem appropriate, including warning the offender or expulsion from the conference with no refund.
     </p>
-    <p>If you are being harassed, notice somebody else is being harassed, or have any other concerns, please contact a member of JJUG staff immediately.</p>
+    <p>If you are being harassed, notice somebody else is being harassed, or have any other concerns, please contact a member of JJUG staff immediately. The staff can be identified as they are wearing an armband. If you do not find any of them, ask someone wearing an event T-shirt.</p>
     <ul>
-      <li>Email: jjug.ccc@gmail.com</li>
-      <li>Web Form: <a href="https://forms.gle/XtFQiZbBwmKeS7h39" target="_blank" rel="noopener noreferrer">https://forms.gle/XtFQiZbBwmKeS7h39</a></li>
+      <li>jjug.ccc@gmail.com</li>
     </ul>
     <p>JJUG will be happy to help participants contact hotel/venue security or local law enforcement, provide escorts, or otherwise assist those experiencing harassment to feel safe for the duration of the event.</p>
-    <p>We value your attendance.</p>
+    <p><b>We value your attendance.</b></p>
   </div>
 );
 
